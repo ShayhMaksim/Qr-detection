7ffdaa670de7262d2958627b79a4480ddbb9bf7a
Maksim <sargaras_m@mail.ru>
2021-04-22 21:55:22 +0300

    All update

--- zbar-opencv-comparison.py
+++ zbar-opencv-comparison.py
@@ -46,8 +46,8 @@ REAL_DATA="data444"
 #   client_socket.send(message.encode())
 
 
-df=pd.DataFrame(columns=['t','x','y','alpha','f'])
-globalDF=pd.DataFrame(columns=['t','x','y'])
+
+globalDF=pd.DataFrame({'t':[],'x':[],'y':[]})
 
 class Point:
   def __init__(self,x,y):
@@ -87,13 +87,13 @@ def display(im, decodedObjects):
 
 
 # нахождене расстояния до Qr-кода
-def distanceCalculate(point1, point2):
+def distanceCalculate(point1, point2,SIDE_OF_QR):
   Len=((point1.x-point2.x)**2+(point1.y-point2.y)**2)**0.5       
   Z=FX*SIDE_OF_QR/(Len)
   return Z
 
 # нахождене расстояния до Qr-кода
-def coordY(point1, point2, X):
+def coordY(point1, point2, X,SIDE_OF_QR):
   Len=((point1.x-point2.x)**2+(point1.y-point2.y)**2)**0.5       
   y=SIDE_OF_QR*(X-CX)/Len
   return y
@@ -105,8 +105,8 @@ def getCenter(point1, point2):
   p.y=(point1.y)+((point2.y-point1.y))/2
   return p
 
-def distanceCalculate2(point1,point2,H):
-  Z=distanceCalculate(point1,point2)#/math.cos(angle)
+def distanceCalculate2(point1,point2,H,SIDE_OF_QR):
+  Z=distanceCalculate(point1,point2,SIDE_OF_QR)#/math.cos(angle)
   L = (Z**2 - H**2)**0.5
   return L
 
@@ -144,107 +144,99 @@ def SMA(y,count):
     return y
 
 
+def SingleData(inputImage,decodedObjects,textStep):
+    zbarData = decodedObjects.data
+    arr = list(map(float, zbarData.split()))
+    SIDE_OF_QR = arr[0]
+    H_QR = arr[1] - H_CAMERA
+    cv2.putText(inputImage, "ZBAR : {}".format(zbarData), (10, 50+textStep), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 1, cv2.LINE_AA)
+    polygon = decodedObjects.polygon
+
+
+    data=polygon[:]
+    if ((polygon[0].y + 30)<(polygon[1].y)):
+      data=polygon[:]
+      key=False
+    else:
+      key=True
+      data[0]=polygon[3]
+      data[1]=polygon[0]
+      data[2]=polygon[1]
+      data[3]=polygon[2]
+      # cv2.putText(inputImage, f"Rotated !!!", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2, cv2.LINE_AA)            
+          
+
+      # cv2.circle(inputImage,data[0],20,(0,255,0),5)# зеленый - левый вверх
+      # cv2.circle(inputImage,data[1],20,(255,0,0),5) # синий - левый низ
+      # cv2.circle(inputImage,data[2],20,(0,0,255),5) # red - правый низ
+      # cv2.circle(inputImage,data[3],20,(0,0,0),5)# black - правый верх
+        
+        
+    centerTop=getCenter(data[0], data[3])
+    centerBottom=getCenter(data[1], data[2])
+
+
+    a = distanceCalculate2(data[0], data[1], H_QR,SIDE_OF_QR)
+    b = distanceCalculate2(centerTop, centerBottom, H_QR,SIDE_OF_QR)
+    c = SIDE_OF_QR/2
+    d = distanceCalculate2(data[2], data[3], H_QR,SIDE_OF_QR)
+    cosA = (a**2 - b**2 - c**2)/(-2*b*c)
+    cosB = (d**2 - b**2 - c**2)/(-2*b*c)
+    A = np.arccos(cosA)
+    B = np.arccos(cosB)
+
+    Arg=B+(np.pi-A-B)/2
+
+        
+    b = mean([a,b,d])
+    b=b/math.sin(Arg)
+    x = b *math.sin(Arg)  
+    y = b *math.cos(Arg) - (1-math.cos(Arg))*coordY(centerTop, centerBottom,(centerTop.x+centerBottom.x)/2.,SIDE_OF_QR)  #math.cos(Arg)
+    y=SMA(y,10)
+    b = (x**2+y**2)**0.5
+    Arg=math.atan2(x,y)
+
+    cv2.putText(inputImage, f"Distance = {round(b,3)}, Alpha = {round(Arg,3)}", (10, 70+textStep), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2, cv2.LINE_AA)     
+    cv2.putText(inputImage, f"X = {round(x, 3)}, Y = {round(y,3)} ", (10, 90+textStep), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2, cv2.LINE_AA)
+
+    globalX=x*np.cos(np.pi/180*arr[4])-y*np.sin(np.pi/180*arr[4])+arr[2]
+    globalY=-x*np.sin(np.pi/180*arr[4])+y*np.cos(np.pi/180*arr[4])+arr[3]
+    cv2.putText(inputImage, f"X(gl) = {round(globalX, 3)}, Y(gl) = {round(globalY,3)} ", (10, 110+textStep), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2, cv2.LINE_AA)
+
+    return globalX,globalY
+
+
 while(1):
     hasFrame, inputImage = cap.read()
     inputImage = cv2.cvtColor(inputImage, cv2.COLOR_BGR2GRAY)
-    #ret, inputImage = cv2.threshold(inputImage, 130, 255, 0)
+    
 
     if not hasFrame:
         break
     decodedObjects = pyzbar.decode(inputImage)
 
+    
     if len(decodedObjects):
-        zbarData = decodedObjects[0].data
-    else:
-        zbarData=''
-    if zbarData:
-        arr = list(map(float, zbarData.split()))
-        SIDE_OF_QR = arr[0]
-        H_QR = arr[1] - H_CAMERA
-        cv2.putText(inputImage, "ZBAR : {}".format(zbarData), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-        polygon = decodedObjects[0].polygon
+      x=0
+      y=0
+      for i in range(0,len(decodedObjects)):
+        x1,y1=SingleData(inputImage,decodedObjects[i],i*110)
+        x+=x1
+        y+=y1
+      x=x/len(decodedObjects)
+      y=y/len(decodedObjects)
 
-
-        data=polygon[:]
-        if ((polygon[0].y + 30)<(polygon[1].y)):
-          data=polygon[:]
-          key=False
-        else:
-          key=True
-          data[0]=polygon[3]
-          data[1]=polygon[0]
-          data[2]=polygon[1]
-          data[3]=polygon[2]
-          # cv2.putText(inputImage, f"Rotated !!!", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2, cv2.LINE_AA)            
-          
-
-        # cv2.circle(inputImage,data[0],20,(0,255,0),5)# зеленый - левый вверх
-        # cv2.circle(inputImage,data[1],20,(255,0,0),5) # синий - левый низ
-        # cv2.circle(inputImage,data[2],20,(0,0,255),5) # red - правый низ
-        # cv2.circle(inputImage,data[3],20,(0,0,0),5)# black - правый верх
-        
-        #mu_0= getMU(getCenter(data[0], data[3]).y + (getCenter(data[1], data[2]).y - getCenter(data[0], data[3]).y)/2)
-        
-        centerTop=getCenter(data[0], data[3])
-        centerBottom=getCenter(data[1], data[2])
-
-        f_0 = getF((centerBottom.x + centerTop.x)/2.)
-
-        a = distanceCalculate2(data[0], data[1], H_QR)
-        b = distanceCalculate2(centerTop, centerBottom, H_QR)
-        c = SIDE_OF_QR/2
-        d = distanceCalculate2(data[2], data[3], H_QR)
-        cosA = (a**2 - b**2 - c**2)/(-2*b*c)
-        cosB = (d**2 - b**2 - c**2)/(-2*b*c)
-        A = np.arccos(cosA)#*180/math.pi
-        B = np.arccos(cosB)
-
-        Arg=B+(np.pi-A-B)/2
-        
-        #print(Arg*180/math.pi)
-        
-        #print(coordY(centerTop, centerBottom,(centerTop.x+centerBottom.x)/2.))
-        b = mean([a,b,d])
-        b=b/math.sin(Arg)
-        x = b *math.sin(Arg)  #math.cos(Arg)
-        y = b *math.cos(Arg) - (1-math.cos(Arg))*coordY(centerTop, centerBottom,(centerTop.x+centerBottom.x)/2.)  #math.cos(Arg)
-        y=SMA(y,10)
-        b = (x**2+y**2)**0.5
-        Arg=math.atan2(x,y)
-
-        
-
-        #x=(b**2-y**2)**0.5
-
-        cv2.putText(inputImage, f"Distance = {round(b,3)}, Alpha = {round(Arg,3)}", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2, cv2.LINE_AA) 
-
-        current_time=time.time()
-        elapsed_time_secs = current_time - time_before
-        time_before=current_time
+      current_time=time.time()
+      elapsed_time_secs = current_time - time_before
+      time_before=current_time
        
-        Y=np.asarray([x,y])
-        x_c,p_c=Correction(Y,elapsed_time_secs,x_p,p_p,b,Arg)
-        x_p,p_p=Prediction(elapsed_time_secs,x_c,p_c)
-        cv2.putText(inputImage, f"X = {round(x, 3)}, Y = {round(y,3)} ", (10, 180), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2, cv2.LINE_AA)
-        
-        cv2.putText(inputImage, f"X = {round(x_c[0], 3)}, Y = {round(x_c[1],3)}, k1 = {round(x_c[2], 3)}, k2 = {round(x_c[3], 3)} ",(10, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 60, 255), 2, cv2.LINE_AA)
 
-        df.loc[index]={'t':elapsed_time_secs,'x':x,'y':y,'alpha':f_0,'f':Arg}
+      globalDF=globalDF.append({'t':elapsed_time_secs,'x':x,'y':y},ignore_index=True)
 
-        #globalX=x_c[0]*np.cos(np.pi/180*arr[4])-x_c[1]*np.sin(np.pi/180*arr[4])+arr[2]
-        #globalY=-x_c[0]*np.sin(np.pi/180*arr[4])+x_c[1]*np.cos(np.pi/180*arr[4])+arr[3]
 
-        #cv2.putText(inputImage, f"X = {round(globalX, 3)}, Y = {round(globalY,3)} ", (10, 220), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 120, 255), 2, cv2.LINE_AA)
+      index=index+1; 
 
-        globalX=x_c[0] 
-        globalY=x_c[1]
-        k1=x_c[2]
-        k2=x_c[3]
-
-        #globalDF[index]={'t':elapsed_time_secs,'x':globalX,'y':globalY}
-        globalDF[index]={'t':elapsed_time_secs,'x':globalX,'y':globalY,'k1':k1,'k2':k2}
-
-        index=index+1; df.to_csv(TEST_NAME); globalDF.to_csv(REAL_DATA)
+      globalDF.to_csv(REAL_DATA)
 
         #message=str(elapsed_time_secs)+','+str(globalX)+','+str(globalY)
         #если не разделять по потокам, то будет смешение данных
@@ -270,102 +262,6 @@ cv2.destroyAllWindows()
 #vid_writer.release()
 
 
-def SigleData(decodedObjects):
-    zbarData = decodedObjects.data
-
-    
-    arr = list(map(float, zbarData.split()))
-    SIDE_OF_QR = arr[0]
-    H_QR = arr[1] - H_CAMERA
-    cv2.putText(inputImage, "ZBAR : {}".format(zbarData), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-    polygon = decodedObjects.polygon
 
 
-    data=polygon[:]
-    if ((polygon[0].y + 30)<(polygon[1].y)):
-      data=polygon[:]
-      key=False
-    else:
-      key=True
-      data[0]=polygon[3]
-      data[1]=polygon[0]
-      data[2]=polygon[1]
-      data[3]=polygon[2]
-      # cv2.putText(inputImage, f"Rotated !!!", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2, cv2.LINE_AA)            
-          
 
-      # cv2.circle(inputImage,data[0],20,(0,255,0),5)# зеленый - левый вверх
-      # cv2.circle(inputImage,data[1],20,(255,0,0),5) # синий - левый низ
-      # cv2.circle(inputImage,data[2],20,(0,0,255),5) # red - правый низ
-      # cv2.circle(inputImage,data[3],20,(0,0,0),5)# black - правый верх
-        
-      #mu_0= getMU(getCenter(data[0], data[3]).y + (getCenter(data[1], data[2]).y - getCenter(data[0], data[3]).y)/2)
-        
-    centerTop=getCenter(data[0], data[3])
-    centerBottom=getCenter(data[1], data[2])
-
-    f_0 = getF((centerBottom.x + centerTop.x)/2.)
-
-    a = distanceCalculate2(data[0], data[1], H_QR)
-    b = distanceCalculate2(centerTop, centerBottom, H_QR)
-    c = SIDE_OF_QR/2
-    d = distanceCalculate2(data[2], data[3], H_QR)
-    cosA = (a**2 - b**2 - c**2)/(-2*b*c)
-    cosB = (d**2 - b**2 - c**2)/(-2*b*c)
-    A = np.arccos(cosA)#*180/math.pi
-    B = np.arccos(cosB)
-
-        Arg=B+(np.pi-A-B)/2
-        
-        #print(Arg*180/math.pi)
-        
-        #print(coordY(centerTop, centerBottom,(centerTop.x+centerBottom.x)/2.))
-        b = mean([a,b,d])
-        b=b/math.sin(Arg)
-        x = b *math.sin(Arg)  #math.cos(Arg)
-        y = b *math.cos(Arg) - (1-math.cos(Arg))*coordY(centerTop, centerBottom,(centerTop.x+centerBottom.x)/2.)  #math.cos(Arg)
-        y=SMA(y,10)
-        b = (x**2+y**2)**0.5
-        Arg=math.atan2(x,y)
-
-        
-
-        #x=(b**2-y**2)**0.5
-
-        cv2.putText(inputImage, f"Distance = {round(b,3)}, Alpha = {round(Arg,3)}", (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2, cv2.LINE_AA) 
-
-        current_time=time.time()
-        elapsed_time_secs = current_time - time_before
-        time_before=current_time
-       
-        Y=np.asarray([x,y])
-        x_c,p_c=Correction(Y,elapsed_time_secs,x_p,p_p,b,Arg)
-        x_p,p_p=Prediction(elapsed_time_secs,x_c,p_c)
-        cv2.putText(inputImage, f"X = {round(x, 3)}, Y = {round(y,3)} ", (10, 180), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 255), 2, cv2.LINE_AA)
-        
-        cv2.putText(inputImage, f"X = {round(x_c[0], 3)}, Y = {round(x_c[1],3)}, k1 = {round(x_c[2], 3)}, k2 = {round(x_c[3], 3)} ",(10, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 60, 255), 2, cv2.LINE_AA)
-
-        df.loc[index]={'t':elapsed_time_secs,'x':x,'y':y,'alpha':f_0,'f':Arg}
-
-        #globalX=x_c[0]*np.cos(np.pi/180*arr[4])-x_c[1]*np.sin(np.pi/180*arr[4])+arr[2]
-        #globalY=-x_c[0]*np.sin(np.pi/180*arr[4])+x_c[1]*np.cos(np.pi/180*arr[4])+arr[3]
-
-        #cv2.putText(inputImage, f"X = {round(globalX, 3)}, Y = {round(globalY,3)} ", (10, 220), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 120, 255), 2, cv2.LINE_AA)
-
-        globalX=x_c[0] 
-        globalY=x_c[1]
-        k1=x_c[2]
-        k2=x_c[3]
-
-        #globalDF[index]={'t':elapsed_time_secs,'x':globalX,'y':globalY}
-        globalDF[index]={'t':elapsed_time_secs,'x':globalX,'y':globalY,'k1':k1,'k2':k2}
-
-        index=index+1; df.to_csv(TEST_NAME); globalDF.to_csv(REAL_DATA)
-
-        #message=str(elapsed_time_secs)+','+str(globalX)+','+str(globalY)
-        #если не разделять по потокам, то будет смешение данных
-        # client_handler=threading.Thread(
-        #   target=handle_client_connection,
-        #   args=(client_sock,message,)
-        # )
-        # client_handler.start()
